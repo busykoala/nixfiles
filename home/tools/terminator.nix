acd039a5151a0c815c76b3c3bd6893748a6a0abd
Matthias Osswald <matthias@busykoala.io>
2024-02-18 18:24:52 +0100

    Start maximized.

--- home/tools/terminator.nix
+++ home/tools/terminator.nix
@@ -1,7 +1,12 @@
 {
   enable = true;
   config = {
-    global_config.borderless = true;
-    profiles.default.background_color = "#1c1c1c";
+    global_config = {
+      borderless = true;
+      window_state = "maximise";
+    };
+    profiles.default = {
+      background_color = "#1c1c1c";
+    };
   };
 }
