0c762af442539fd9814d62ab1fb83c788927e48e
Matthias Osswald <matthias@bespinian.io>
2024-03-04 09:49:39 +0100

    Re-enable alacritty without window max option.
    The option startup_mode='Maximized' led to alacritty not starting anymore. Therefor this commit adds the previous config again but without this config.

--- hosts/schnurrli/home/default.nix
+++ hosts/schnurrli/home/default.nix
@@ -43,5 +43,6 @@ in
     neovim = import ../../../home/tools/neovim.nix { inherit pkgs; };
     tmux = import ../../../home/tools/tmux.nix;
     terminator = import ../../../home/tools/terminator.nix;
+    alacritty = import ../../../home/tools/alacritty.nix;
   };
 }
