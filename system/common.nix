643c29779a9f32f05a1ad3499668d03676febbcc
Matthias Osswald <matthias@bespinian.io>
2024-07-24 18:31:51 +0200

    Remove deprecated sound.enable option.

--- system/common.nix
+++ system/common.nix
@@ -39,7 +39,7 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
+  # sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
